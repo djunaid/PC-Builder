c1b2303ecb4f338002c370689f04953ea13c812c
Danish Junaid <danish.junaid90@gmail.com>
2023-11-28 22:20:56 -0600

    Adding folders for Domain project and classes for Common

---
+++ Domain/Common/BaseEntity.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RecipeBuilder.Domain.Common
+{
+    public abstract class BaseEntity<T> 
+    {
+        public T id { get; set; }
+
+        private readonly List<BaseEvent> _domainEvents = new();
+
+        [NotMapped]
+        public IReadOnlyCollection<BaseEvent> DomainEvents => _domainEvents.AsReadOnly();
+
+        public void AddDomainEvent(BaseEvent domainEvent)
+        {
+            _domainEvents.Add(domainEvent);
+        }
+
+        public void RemoveDomainEvent(BaseEvent domainEvent)
+        {
+            _domainEvents.Remove(domainEvent);
+        }
+
+        public void ClearDomainEvents()
+        {
+            _domainEvents.Clear();
+        }
+    
+
+}
+}
---
+++ Domain/GlobalUsings.cs
@@ -0,0 +1,1 @@
+﻿global using RecipeBuilder.Domain.Common;
