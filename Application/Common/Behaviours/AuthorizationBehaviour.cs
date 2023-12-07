db4ec70602e5cad7678b136ac9281e0d51075fa8
Danish Junaid <danish.junaid90@gmail.com>
2023-12-06 22:50:52 -0600

    adding validation and forbidden exception classes and Authorization BEhavior class

--- Application/Common/Behaviours/AuthorizationBehaviour.cs
+++ Application/Common/Behaviours/AuthorizationBehaviour.cs
@@ -1,17 +1,85 @@
-﻿using MediatR;
+﻿using Application.Common.Exceptions;
+using Application.Common.Interfaces;
+using Application.Common.Security;
+using MediatR;
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Reflection;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace Application.Common.Behaviours
+namespace Application.Common.Behaviours;
+
+public class AuthorizationBehaviour<TRequest, TResponse> : IPipelineBehavior<TRequest, TResponse> where TResponse : notnull
 {
-    public class AuthorizationBehaviour<TRequest, TResponse> : IPipelineBehavior<TRequest, TResponse> where TResponse : notnull
+    private readonly IUser _user;
+    private readonly IIdentityService _identityService;
+
+    public AuthorizationBehaviour(IUser user, IIdentityService identityService)
     {
-        public Task<TResponse> Handle(TRequest request, RequestHandlerDelegate<TResponse> next, CancellationToken cancellationToken)
+        _user = user;
+        _identityService = identityService;
+    }
+
+
+    public async Task<TResponse> Handle(TRequest request, RequestHandlerDelegate<TResponse> next, CancellationToken cancellationToken)
+    {
+        var authorizeAttributes = request.GetType().GetCustomAttributes<AuthorizeAttribute>();
+
+        if (authorizeAttributes.Any())
         {
-            throw new NotImplementedException();
+            // Must be authenticated user
+            if (_user.Id == null)
+            {
+                throw new UnauthorizedAccessException();
+            }
+
+            // Role-based authorization
+            var authorizeAttributesWithRoles = authorizeAttributes.Where(a => !string.IsNullOrEmpty(a.Roles));
+
+            if (authorizeAttributesWithRoles.Any())
+            {
+                var authorized = false;
+
+                foreach (var roles in authorizeAttributesWithRoles.Select(a => a.Roles.Split(',')))
+                {
+                    foreach (var role in roles)
+                    {
+                        var isInRole = await _identityService.IsInRoleAsync(_user.Id, role.Trim());
+
+                        if (isInRole)
+                        {
+                            authorized = true;
+                            break;
+                        }
+                    }
+                }
+
+                if (!authorized)
+                {
+                    throw new ForbiddenAccessException();
+                }
+            }
+
+            // Policy-based authorization
+            var authorizeAttributeWithPolicies = authorizeAttributes.Where(a => !string.IsNullOrEmpty(a.Policy));
+            if (authorizeAttributeWithPolicies.Any())
+            {
+                foreach (var policy in authorizeAttributeWithPolicies.Select(a => a.Policy))
+                {
+                    var authorized = await _identityService.AuthorizeAsync(_user.Id, policy);
+
+                    if (!authorized)
+                    {
+                        throw new ForbiddenAccessException();
+                    }
+                }
+            }
+
         }
+
+        // User is authorized / authorization not required
+        return await next();
     }
 }
---
+++ Application/Common/Security/AuthorizeAttribute.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Application.Common.Security
+{
+    public class AuthorizeAttribute : Attribute
+    {
+
+        public AuthorizeAttribute() { }
+
+        /// <summary>
+        /// Gets or sets a comma delimited list of roles that are allowed to access the resource.
+        /// </summary>
+        public string Roles { get; set; } = string.Empty;
+
+        /// <summary>
+        /// Gets or sets the policy name that determines access to the resource.
+        /// </summary>
+        public string Policy { get; set; } = string.Empty;
+    }
+}
